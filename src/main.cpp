9e808147b463b7f33856aa2d016f87a2c51994aa
Nigel Armstrong <legonigel@gmail.com>
2015-12-31 01:56:22 -0500

    Main now does stuff
    It now makes an asynchronous request to the example api server then
    spits out the values for the grid size. Pretty awesome, right?!

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,53 @@
 //Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 ///////////////////////////////////////////////////////////////////////////////
 
+#include "main.h"
+
+// C++ REST SDK
+#include <cpprest/http_client.h>
+
+#define HOST "http://private-6dd53-jamapi.apiary-mock.com/"
+
+using namespace web;
+using namespace web::http;
+using namespace web::http::client;
+
+auto grid_size_thread(http_client &my_client)
+{
+  return my_client.request(methods::GET, U("/api/grid")).then([](http_response resp)
+  {
+    return resp.extract_json();
+  }).then([](json::value grid_json)
+  {
+    grid_t my_grid;
+    if (!grid_json[U("x")].is_null()){
+      my_grid.x = grid_json[U("x")].as_integer();
+    } else {
+      //We had an error in the request
+      //For now, we will set the value to undefined
+      my_grid.x = GRID_UNDEFINED;
+    }
+    if (!grid_json[U("y")].is_null()){
+      my_grid.y = grid_json[U("y")].as_integer();
+    } else {
+      //We had an error in the request
+      //For now, we will set the value to undefined
+      my_grid.y = GRID_UNDEFINED;
+    }
+    return my_grid;
+  });
+}
+
 int main(int argc, char *argv[]){
+
+  http_client my_client(U(HOST));
+
+  auto grid_size_worker = grid_size_thread(my_client);
+
+  grid_t my_grid = grid_size_worker.get();
+
+  std::cout << "X:" << my_grid.x << std::endl;
+  std::cout << "Y:" << my_grid.y << std::endl;
+
   return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#define GRID_UNDEFINED -1
+
+struct grid_t
+{
+  int x, y;
+};
+
+#endif /* MAIN_H */
