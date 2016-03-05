e29c6d140436777bf0370645bb2ee6028247666e
Nigel Armstrong <legonigel@gmail.com>
2016-03-04 23:56:47 -0500

    Added additional printing
     - Printing of entire map, including all obstacles, and the path
     - Fix path printing to fixed width
     - Add function for printing out moving obstacle points

--- src/main.cpp
+++ src/main.cpp
@@ -36,19 +36,61 @@ using namespace web;
 using namespace web::http;
 using namespace web::http::client;
 
-void print_env(env_data_t my_env_data)
+void print_env(env_data_t my_env_data, point_char_map moving_obs, std::vector<sbpl_xy_theta_pt_t> path)
 {
     std::cout << "Grid"  << std::endl << std::endl;
+    point_char_map path_points = point_char_map();
+    for (unsigned int i = 0; i < path.size(); i++) {
+        int pt_x = (int)path.at(i).x;
+        int pt_y = (int)path.at(i).y;
+        int pt_ang = ((int)((RAD_TO_DEG(path.at(i).theta) + 22.5) / 45)) % 8;
+        std::pair<int, int> point = std::pair<int, int>(pt_x, pt_y);
+        //Points 0-7 0 for staight, 1 for angle, etc.
+        if (path_points.count(point) == 1) {
+            int existing_ang = path_points.at(point);
+            pt_ang = (pt_ang + existing_ang) / 2;
+        }
+        path_points[point] = pt_ang;
+    }
     for(int i = 0; i < my_env_data.height; i++) {
         for(int j = 0; j < my_env_data.width; j++) {
+            std::pair<int, int> point = std::pair<int, int>(j, i);
             if(j == my_env_data.start_x && i == my_env_data.start_y) {
                 std::cout << "S";
             } else if (j == my_env_data.end_x && i == my_env_data.end_y) {
                 std::cout << "G";
+            } else if (path_points.count(point) == 1) {
+                switch (path_points.at(point)) {
+                case 0:
+                case 4:
+                    std::cout << "-";
+                    break;
+                case 1:
+                case 5:
+                    std::cout << "\\";
+                    break;
+                case 2:
+                case 6:
+                    std::cout << "|";
+                    break;
+                case 3:
+                case 7:
+                    std::cout << "/";
+                    break;
+                }
+            } else if (moving_obs.count(point) == 1) {
+                unsigned char obs_val = moving_obs.at(point);
+                if (obs_val == 255) {
+                    std::cout << "O";
+                } else if(obs_val == 0) {
+                    std::cout << " ";
+                } else {
+                    std::cout << (obs_val / 26);
+                }
             } else if(my_env_data.grid_2d[j + i * my_env_data.width] == 0) {
                 std::cout << " ";
             } else if (my_env_data.grid_2d[j + i * my_env_data.width] < 255) {
-                std::cout << (my_env_data.grid_2d[j + i * my_env_data.width] / 29);
+                std::cout << (my_env_data.grid_2d[j + i * my_env_data.width] / 26);
             } else {
                 std::cout << "O";
             }
@@ -72,8 +114,17 @@ void print_env_const(env_constants_t my_env_const)
 void print_path(std::vector<sbpl_xy_theta_pt_t> path)
 {
     for (unsigned int i = 0; i < path.size(); i++) {
-        printf("%.3f %.3f %.3f\n", path.at(i).x, path.at(i).y, RAD_TO_DEG(path.at(i).theta));
+        printf("%3.3f %3.3f %3.3f\n", path.at(i).x, path.at(i).y, RAD_TO_DEG(path.at(i).theta));
     }
+}
+
+void print_moving_obs_points(point_char_map mov_obs)
+{
+    std::cout << "Printing Moving Obstacles" << std::endl;
+    for (auto obs : mov_obs) {
+        std::cout << obs.first.first << ", " << obs.first.second << " " << (int)obs.second << std::endl;
+    }
+
 }
 
 int main(int argc, char *argv[])
@@ -122,9 +173,11 @@ int main(int argc, char *argv[])
 
     std::cout << "Plan returned: " << has_path << std::endl;
 
+
     if(has_path) {
         //Print the path to stdout
-        print_path(my_planner.get_path());
+        //print_path(my_planner.get_path());
+        print_env(my_env_data, moveing_obs_pts, my_planner.get_path());
     }
 
     return 0;
